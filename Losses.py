fixed Slicing to deal with memory issues and debugged 3dDice computation. Results very low, crosscheck needed.

Losses.py
@@ -170,9 +170,9 @@ def batchGDL(probs: Tensor, target: Tensor, binary: bool = False):
     tc = target.type(torch.float32)
 
     intersection: Tensor = einsum("bcwh...,bcwh...->b", pc, tc)
-    union: Tensor = einsum("bcwh...->b", tc) #ne bo nikoli 0. zmeri bo == numel. 
+    union: Tensor = einsum("bcwh...->b", pc) + einsum("bcwh...->b", tc) #ne bo nikoli 0. zmeri bo == numel. 
 
-    divided: Tensor = intersection / union
+    divided: Tensor = 2*intersection / union
    
     return divided
 

Postprocessing.py
@@ -9,7 +9,7 @@ from helpers import flatten_one_hot, get_one_hot, CenterCropTensor, CenterCropTe
 import matplotlib.patches as mpatches
 import matplotlib
 import random
-from Losses import DicePerClass, AllDices, DicePerClassBinary
+from Losses import DicePerClass, AllDices, DicePerClassBinary, batchGDL
 #matplotlib.use('Agg')
 
 
@@ -196,40 +196,7 @@ def plotOutput(params, datafolder, pids, doeval=True, take20=None):
 from pathlib import Path
 from tqdm import tqdm
 import re
-
-def ConstructSubject(out, maxi, maxj, maxk):
-    #out is sized B x 7 x h x w (x d), depending if 2 or 3D
-    is3D = out.ndim==5
-    #you kow how they were cut (in the POEM_eval dataset), so join them back in the same way. 
-    if not is3D:
-        return out.permute(1,2,0,3) 
-    
-    subject = torch.zeros((7, (maxi+1)*34+16, (maxj+1)*34+16, (maxk+1)*34+16))
-    #else we're in 3D and need to reconstruct subject. 
-    index = -1
-    if out.shape[2]==50: #unet and Pnet, sizes are the same as input
-        for i in range(maxi):
-            for j in range(maxj):
-                for k in range(maxk):
-                    index += 1
-                    subject[:, i*34+16*(i>0):i*34+50, 
-                               j*34+16*(j>0):j*34+50, 
-                               k*34+16*(k>0):k*34+50] = out[index, :, 16*(i>0):,16*(j>0):,16*(k>0):] 
-                               #for now we don't average overlapping outputs. simply use the first one.
-                               
-                    
-    else: #out.shape[2]=34, deepMedic
-        for i in range(maxi):
-            for j in range(maxj):
-                for k in range(maxk):
-                    index +=1
-                    subject[:, 8+i*34:(i+1)*34+8, 
-                               8+j*34:(j+1)*34+8,
-                               8+k*34:(k+1)*34+8] = out[index, ...]
-    
-    return subject
-    
-
+from natsort import natsorted 
 
 
 
@@ -242,7 +209,8 @@ def doInference(subject, folder, network, best_ep=True):
     #now get network
     Arg = {'network': None, 'n_class':7, 'in_channels':2, 'lower_in_channels':2, 'extractor_net':'resnet34'}
     netfiles = Path("RESULTS", folder, network)
-    with open(netfiles.glob("*_args.txt")[0], "r") as ft:
+    argfiles = list(netfiles.glob("*_args.txt"))
+    with open(argfiles[0], "r") as ft:
         args = ft.read().splitlines()
     tmpargs = [i.strip('--').split("=") for i in args if ('--' in i and '=' in i)]
     chan1, whichin1 = getchn(args, 'in_chan')
@@ -254,12 +222,13 @@ def doInference(subject, folder, network, best_ep=True):
     #overwrite if given in file:
     Arg.update(args)
     use_in2 = Arg['network']=='DeepMedic'
-    device = torch.device('cuda') #need speed now
+    device = torch.device('cpu') #need speed now
 
     net = getattr(Networks, Arg['network'])(Arg['in_channels'], Arg['n_class'], Arg['lower_in_channels'], Arg['extractor_net'], in3D)
     net = net.float()
     #now we can load learned params:
-    path_to_net = netfiles.glob("*_bestepoch")[0] if best_ep else Path(netfiles, network)
+    bestepfiles = list(netfiles.glob("*_bestepoch"))
+    path_to_net = bestepfiles[0] if best_ep else Path(netfiles, network)
     loaded = torch.load(path_to_net, map_location=lambda storage, loc: storage)
     net.load_state_dict(loaded['state_dict'])
     net.eval()   
@@ -275,11 +244,13 @@ def doInference(subject, folder, network, best_ep=True):
         in2 = torch.stack([torch.from_numpy(np.load(i2)).float() for i2 in subjekti2], dim=0)
         data.append(in2[:, whichin2, ...])
     
-    bigslice = 8
+    bigslice = 5
     out = []
     for start in range(0, len(data[0]), bigslice):
         in_batch = [datum[start:start+bigslice, ...].to(device) for datum in data]
-        out.append(net(*in_batch)) #.to('cpu')?
+       # print([i.shape for i in in_batch])
+        tmp = net(*in_batch).detach().cpu()
+        out.append(tmp)
 
     return torch.cat(out) #concatenate so it's like one big batch. 
 
@@ -290,26 +261,29 @@ def get3Ddice(PID, in3d, folder, network, best_ep=True):
         best_ep = evaluate at best epoch? If false, it evals after complete training."""
 
     if in3d:
-        subject = Path('POEM_eval', 'TriD', 'in1').glob(f"*{PID}*.npy")
-        allnrs = np.array([[int(x) for x in re.findall(r"_([0-9]+)", p.name)] for p in subject]).max(axis=0)
-        maxi,maxj,maxk = allnrs
+        subject = [i for i in Path('POEM_eval', 'TriD', 'in1').glob(f"*{PID}*.npy")]
     else:
-        subject = Path('POEM_eval', 'TwoD', 'in1').glob(f"*{PID}*.npy")
-        maxi,maxj,maxk = None,None,None
-    subject.sort()
+        subject = [i for i in Path('POEM_eval', 'TwoD', 'in1').glob(f"*{PID}*.npy")]
+       
+    subject = natsorted(subject)
     #do inference to get batched output:
     out = doInference(subject, folder, network, best_ep)
 
-    #now glue/build outputs back into a 3D person:
-    person = ConstructSubject(out, maxi, maxj, maxk)
     #calculate Dice:
-    gt = torch.from_numpy(np.load(Path('POEM_eval', 'GTs').glob(f"*{PID}*.npy")[0]))
-    _,s0,s1,s2 = gt.shape
-    print(f"\ngt: {gt.shape}, person: {person.shape}\n")
-    person = person[:, :s0, :s1, :s2]
-    Dices = DicePerClassBinary(person, gt) #TODO: in case of deepmedic, the outer 8pix should be ignored in calc...?
+    if in3d:
+        gtpaths = natsorted(list(Path('POEM_eval', 'GTs_3D').glob(f"*{PID}*.npy")))
+    else:
+        gtpaths = natsorted(list(Path('POEM_eval', 'GTs_2D').glob(f"*{PID}*.npy")))
+    gt = [torch.from_numpy(np.load(i)) for i in gtpaths]
+    gt = torch.stack(gt)
+    
+   # print([[i.name, j.name] for i,j in zip(subject, gtpaths)])
+    gt, _ = CenterCropTensor3d(gt, out) #in case of deepmedic, gt should be cropped to out size
+    
+    Dices = batchGDL(out.transpose(0,1), gt.transpose(0,1), binary=True) #DicePerClassBinary(out, gt) <-this avges over all patches... not cool
 
     return Dices 
+
 
 def Calc3Ddices(folder, network, best_ep=True):
     """ folder = results folder, from where your net will be loaded

Slicing.py
@@ -104,12 +104,19 @@ def cutPOEM2D(patch_size, outpath, make_subsampled=True, add_dts=True, sliced=1,
         pathlib.Path(f"{outpath}/{i}").mkdir(parents=True, exist_ok=True)
 
     #POEM SLICING
-    gt_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/procesiranDataset/POEM_segment_all/converted/CroppedSegmNew*")
-    wat_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/procesiranDataset/POEM_segmentation_data_fatwat/converted/cropped*_wat*")
-    fat_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/procesiranDataset/POEM_segmentation_data_fatwat/converted/cropped*_fat*")
-    dtx_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/distmaps/*x.nii")
-    dty_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/distmaps/*y.nii")
-    mask_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/procesiranDataset/POEM_segmentation_data_fatwat/converted/cropped*_mask.nii")
+    #gt_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/procesiranDataset/POEM_segment_all/converted/CroppedSegmNew*")
+    #wat_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/procesiranDataset/POEM_segmentation_data_fatwat/converted/cropped*_wat*")
+    #fat_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/procesiranDataset/POEM_segmentation_data_fatwat/converted/cropped*_fat*")
+    #dtx_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/distmaps/*x.nii")
+    #dty_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/distmaps/*y.nii")
+    #mask_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/procesiranDataset/POEM_segmentation_data_fatwat/converted/cropped*_mask.nii")
+
+    gt_paths = glob("POEM/segms/CroppedSegmNew*")
+    wat_paths = glob("POEM/watfat/cropped*_wat*")
+    fat_paths = glob("POEM/watfat/cropped*_fat*")
+    dtx_paths = glob("POEM/distmaps/*x.nii")
+    dty_paths = glob("POEM/distmaps/*y.nii")
+    mask_paths = glob("POEM/masks/cropped*_mask.nii")
 
     gt_paths.sort()
     wat_paths.sort()
@@ -248,6 +255,14 @@ def cutPOEM3D(patch_size, outpath, make_subsampled=True, add_dts=True, sampling=
     dty_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/distmaps/*y.nii")
     mask_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/procesiranDataset/POEM_segmentation_data_fatwat/converted/cropped*_mask.nii")
 
+    #gt_paths = glob("POEM/segms/CroppedSegmNew*")
+    #wat_paths = glob("POEM/watfat/cropped*_wat*")
+    #fat_paths = glob("POEM/watfat/cropped*_fat*")
+    #dtx_paths = glob("POEM/distmaps/*x.nii")
+    #dty_paths = glob("POEM/distmaps/*y.nii")
+    #mask_paths = glob("POEM/masks/cropped*_mask.nii")
+
+
     gt_paths.sort()
     wat_paths.sort()
     fat_paths.sort()
@@ -380,6 +395,13 @@ def cutPOEMslices():
     dtx_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/distmaps/*x.nii")
     dty_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/distmaps/*y.nii")
    
+    #gt_paths = glob("POEM/segms/CroppedSegmNew*")
+    #wat_paths = glob("POEM/watfat/cropped*_wat*")
+    #fat_paths = glob("POEM/watfat/cropped*_fat*")
+    #dtx_paths = glob("POEM/distmaps/*x.nii")
+    #dty_paths = glob("POEM/distmaps/*y.nii")
+    #mask_paths = glob("POEM/masks/cropped*_mask.nii")
+
     gt_paths.sort()
     wat_paths.sort()
     fat_paths.sort()
@@ -481,20 +503,28 @@ def cutEval():
     patch_size = 50
     batch_size2 = 8
     batch_size3 = 5
-    GTs = pathlib.Path('POEM_eval', 'GTs')
-    GTs.mkdir(parents=True, exist_ok=True)
+    GTs2 = pathlib.Path('POEM_eval', 'GTs_2D')
+    GTs3 = pathlib.Path('POEM_eval', 'GTs_3D')
+    GTs2.mkdir(parents=True, exist_ok=True)
+    GTs3.mkdir(parents=True, exist_ok=True)
     for i in ['in1','in2']:
         pathlib.Path(outpath2, i).mkdir(parents=True, exist_ok=True)
         pathlib.Path(outpath3, i).mkdir(parents=True, exist_ok=True)
 
     #POEM SLICING
-    gt_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/procesiranDataset/POEM_segment_all/converted/CroppedSegmNew*")
-    wat_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/procesiranDataset/POEM_segmentation_data_fatwat/converted/cropped*_wat*")
-    fat_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/procesiranDataset/POEM_segmentation_data_fatwat/converted/cropped*_fat*")
-    dtx_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/distmaps/*x.nii")
-    dty_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/distmaps/*y.nii")
-    mask_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/procesiranDataset/POEM_segmentation_data_fatwat/converted/cropped*_mask.nii")
+    #gt_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/procesiranDataset/POEM_segment_all/converted/CroppedSegmNew*")
+    #wat_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/procesiranDataset/POEM_segmentation_data_fatwat/converted/cropped*_wat*")
+    #fat_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/procesiranDataset/POEM_segmentation_data_fatwat/converted/cropped*_fat*")
+    #dtx_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/distmaps/*x.nii")
+    #dty_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/distmaps/*y.nii")
+    #mask_paths = glob("/home/eva/Desktop/research/PROJEKT2-DeepLearning/procesiranDataset/POEM_segmentation_data_fatwat/converted/cropped*_mask.nii")
 
+    gt_paths = glob("POEM/segms/CroppedSegmNew*")
+    wat_paths = glob("POEM/watfat/cropped*_wat*")
+    fat_paths = glob("POEM/watfat/cropped*_fat*")
+    dtx_paths = glob("POEM/distmaps/*x.nii")
+    dty_paths = glob("POEM/distmaps/*y.nii")
+    mask_paths = glob("POEM/masks/cropped*_mask.nii")
    
     gt_paths.sort()
     wat_paths.sort()
@@ -509,10 +539,15 @@ def cutEval():
 
     nb_class = 7
 
+    PIDS = ['500026', '500061', '500075', '500117', '500242', '500268', '500288', '500291', 
+            '500316', '500346', '500347', '500348', '500354', '500433', '500487']
+    
     for w,f,g,dx,dy,m in zip(wat_paths, fat_paths, gt_paths, dtx_paths, dty_paths, mask_paths):
         PIDs = [re.findall(r"500[0-9]+", ppp)[0] for ppp in [w,f,g,dx,dy,m]]
         assert len(np.unique(PIDs)) == 1
         PID = PIDs[0]
+        if PID not in PIDS:
+            continue
         print(f"Slicing nr {PID}...")
         wat = nib.load(w).get_fdata()
         fat = nib.load(f).get_fdata()
@@ -534,32 +569,37 @@ def cutEval():
 
         #SAVE GT
         gt = get_one_hot(gt, nb_class) #new size C x H x W x D
-        np.save(pathlib.Path(GTs, f"subj{PID}.npy"), gt)
+        #np.save(pathlib.Path(GTs, f"subj{PID}.npy"), gt)
         #SAVE 2D SLICES
         for s in range(wat.shape[1]):
             np.save(pathlib.Path(outpath2, 'in1', f"subj{PID}_{s}.npy"), np.squeeze(allin[:,:,s,:]))
             np.save(pathlib.Path(outpath2, 'in2', f"subj{PID}_{s}.npy"), np.squeeze(allin[:,0::3,s,0::3]))
+            np.save(pathlib.Path(GTs2, f"subj{PID}_{s}.npy"), np.squeeze(gt[:,:,s,:]))
             
         #SAVE 3D PATCHES
         #for easier subsampl. data, first pad with 0s:
-        allin = np.pad(allin, ((0,), (16,), (16,), (16,)))
+        allin = np.pad(allin, ((0,), (16,), (16,), (16,)), mode='constant')
+        gt = np.pad(gt, ((0,), (16,), (16,), (16,)), mode='constant')
         for i in range(16,wat.shape[0]+16,(patch_size-16)):
             for j in range(16,wat.shape[1]+16,(patch_size-16)):
                 for k in range(16,wat.shape[2]+16,(patch_size-16)):
                     tmp_in1 = allin[:, i:i+50, j:j+50, k:k+50]
                     tmp_in2 = allin[:, i-16:i+66:3, j-16:j+66:3, k-16:k+66:3]
+                    tmp_gt = gt[:, i:i+50, j:j+50, k:k+50]
                     
                   #  print(f"in1: {tmp_in1.shape}, in2: {tmp_in2.shape}")
                     _, s10, s11, s12 = tmp_in1.shape
                     _, s20, s21, s22 = tmp_in2.shape
-                    tmp_in1 = np.pad(tmp_in1, ((0,0),(0,50-s10), (0,50-s11), (0,50-s12)))
-                    tmp_in2 = np.pad(tmp_in2, ((0,0),(0,28-s20), (0,28-s21), (0,28-s22)))
+                    tmp_in1 = np.pad(tmp_in1, ((0,0),(0,50-s10), (0,50-s11), (0,50-s12)), mode='constant')
+                    tmp_gt = np.pad(tmp_gt, ((0,0),(0,50-s10), (0,50-s11), (0,50-s12)), mode='constant')
+                    tmp_in2 = np.pad(tmp_in2, ((0,0),(0,28-s20), (0,28-s21), (0,28-s22)), mode='constant')
                   #  print(f"NEW: \t {tmp_in1.shape}, in2: {tmp_in2.shape}")
 
                     np.save(pathlib.Path(outpath3, 'in1', f"subj{PID}_{i}_{j}_{k}.npy"), 
                             tmp_in1)
                     np.save(pathlib.Path(outpath3, 'in2', f"subj{PID}_{i}_{j}_{k}.npy"), 
                             tmp_in2)
+                    np.save(pathlib.Path(GTs3, f"subj{PID}_{i}_{j}_{k}.npy"), tmp_gt)
 
             
         