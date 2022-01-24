turned rolling average back on, todo: fix slice sizes in computeDice3D (for saving results). Mismatch when comparing to GT imgs.

Networks.py
@@ -29,18 +29,18 @@ class DoubleConv(nn.Module):
         if TriD:
             self.block = nn.Sequential(
                 nn.Conv3d(in_channels, mid_channels, kernel_size=kernel_size, padding=padding, stride=stride, dilation=dilation, bias=False), #change padding?
-                nn.BatchNorm3d(out_channels, track_running_stats=False),
+                nn.BatchNorm3d(out_channels), #, track_running_stats=False),
                 nn.PReLU(), #(P)relu?
                 nn.Conv3d(mid_channels, out_channels, kernel_size=kernel_size, padding=padding, dilation=dilation, bias=False),
-                nn.BatchNorm3d(out_channels, track_running_stats=False),
+                nn.BatchNorm3d(out_channels), #, track_running_stats=False),
             )
         else:
             self.block = nn.Sequential(
                 nn.Conv2d(in_channels, mid_channels, kernel_size=kernel_size, padding=padding, stride=stride, dilation=dilation, bias=False), #change padding?
-                nn.BatchNorm2d(out_channels, track_running_stats=False),
+                nn.BatchNorm2d(out_channels), #, track_running_stats=False),
                 nn.PReLU(), #(P)relu?
                 nn.Conv2d(mid_channels, out_channels, kernel_size=kernel_size, padding=padding, dilation=dilation, bias=False),
-                nn.BatchNorm2d(out_channels, track_running_stats=False),
+                nn.BatchNorm2d(out_channels), #, track_running_stats=False),
             )
         self.act = nn.PReLU()
     
@@ -466,8 +466,9 @@ class DilatedResNet(nn.Module):
 #################loading pretrained nets
 
 def load_weights_sequential(target, source_state):
-    model_to_load= {k: v for k, v in source_state.items() if k in target.state_dict().keys()}
-    target.load_state_dict(model_to_load)
+    #model_to_load= {k: v for k, v in source_state.items() if k in target.state_dict().keys()}
+    #target.load_state_dict(model_to_load, strict=False) #the strict thingie does the same!
+    target.load_state_dict(source_state, strict=False) #the strict thingie does the same!
 
 
 def resnet18(pretrained=True):

Postprocessing.py
@@ -11,7 +11,7 @@ import nibabel as nib
 from typing import List, Tuple, Dict, Any, Union
 from scipy.ndimage import distance_transform_edt as dt_edt
 from slicer import Slicer
-
+#%%
 
 
 def getchn(args: List[str], string:str) -> Tuple[int, List[int]]:
@@ -109,9 +109,9 @@ def loadSubject(pid:int, leavebckg:int) -> Tuple[np.ndarray, np.ndarray, np.ndar
 #%%
 def Compute3DDice(PID:Union[int, List[int]], netparams:str, patchsize:int, 
             batch:int = 10, bydim:int = 1, doeval:bool = True, 
-            dev:str = 'cpu', step:int=0, saveout:bool=False) -> List[float]:
+            dev:str = 'cpu', step:int=0, saveout:bool=False, savename:str='x') -> List[float]:
     #OBS: in case of deepmed, patchsize means the size of output patch!
-    #(i.e. if patchsize=9, the input to network will be 25x25)
+    #(i.e. if patchsize=9, the input to network will be 25x25) <-but this done in the code
     #step = in what steps you take patches. if ==0, you take nonoverlapping ones. if K, patch starts 
     # at prev_patch_start+K.
     #saveout = whether we save the ful subject output. (for viewing and debugging)
@@ -120,9 +120,13 @@ def Compute3DDice(PID:Union[int, List[int]], netparams:str, patchsize:int,
     net, in1, in2, in3D = getNetwork(netparams, dev)
     if doeval:
         net.eval()
+    else:
+        net.train()
     device = torch.device(dev)
     print('Net loaded.')
     # CUT AND EVAL: loop through cutting smaller pieces, moving to torch and eval
+    if isinstance(PID, int): 
+        PID=[PID]
     segmented = torch.zeros((1,7), device=dev)
     existing = torch.zeros((1,7), device=dev)
     intersec = torch.zeros((1,7), device=dev) #these three needed to gather results, for post dice compute
@@ -145,7 +149,6 @@ def Compute3DDice(PID:Union[int, List[int]], netparams:str, patchsize:int,
         TensorCropping = CenterCropTensor
 
     # LOAD DATA:
-    if isinstance(PID, int): PID=[PID]
     for idx, pid in enumerate(PID):
         #set accumulators to 0:
         segmented.zero_()
@@ -159,13 +162,17 @@ def Compute3DDice(PID:Union[int, List[int]], netparams:str, patchsize:int,
         mask = np.pad(mask, padding[1:], mode='constant')
         gt = np.pad(gt, padding, mode='constant')
         allin = np.pad(allin, paddingall, mode='constant')
-
-        empty_subj = torch.zeros(allin.shape[1:]) #cause we dont need channels
-    
+        print((size_full, gt.shape))
+        empty_subj = torch.zeros(gt.shape[1:]) #allin.shape[1:]) #cause we dont need channels
+        
         slicer = Slicer(size_full, patchsize, in1, in2, in3D, bydim, step) #return string slice, include all channels
         # for cutting out the middle part based on step:
         #slice((sf-step)//2, sf-np.ceil((sf-step)/2))
         slicing = "".join([f'.narrow({idx}, {(patchsize-step)//2}, {step})' for idx in range(2,(4+in3D))]) if step>0 else ""
+        paddingup = [0,patchsize-step]*3
+        if not in3D:
+            paddingup[-1-bydim*2] = 0
+
         print(f'Eval on subj{pid}...')
         with torch.no_grad():
             while slicer.todo>0:
@@ -190,11 +197,7 @@ def Compute3DDice(PID:Union[int, List[int]], netparams:str, patchsize:int,
                 #dices = AllDices(out, target_oh)
                 maske = torch.from_numpy(maske).squeeze().unsqueeze(1).float().to(device)
                 
-                #save output if required
-                if saveout: #whats faster, simply saving to an existing tensor, or iffing every loop??
-                    for idd, slajs in gtslices:
-                        eval(f'empty_subj[{slajs}] = torch.argmax(out[idd,...], dim=0)')
-
+                
                 #cut only the middle part of OUT, MASKE and TARGET_OH for eval (depending on the step size)
                 maske = eval('maske'+slicing)
                 target_oh = eval('target_oh'+slicing)
@@ -204,16 +207,29 @@ def Compute3DDice(PID:Union[int, List[int]], netparams:str, patchsize:int,
                 segmented += torch.sum(out*maske, axis=axes)
                 existing += torch.sum(target_oh*maske, axis=axes)
                 intersec += torch.sum(target_oh*maske*out, axis=axes)
-                #TODO: save sums in segmented, existing, intersec. Also use mask? (then it needs cutting!!)
-    
+
+                #save output if required
+                if saveout: #whats faster, simply saving to an existing tensor, or iffing every loop??
+                    for idd, slajs in enumerate(gtslices):
+                        tmp = torch.argmax(out[idd,...], dim=0)
+                        if not in3D:
+                            tmp = tmp.unsqueeze(bydim)
+                       # print(tmp.shape)
+                        tmp = torch.nn.functional.pad(tmp, paddingup)
+                        eval(f'empty_subj[{slajs[2:]}].copy_(tmp)')
+
+                
         #all saved, now calc actual dices:
         Dices[idx, :] = 2*intersec/(existing+segmented) #calc dice from the gathering lists
         if saveout:
             #save img as npy.
-            np.save(f'out{pid}.npy', empty_subj.cpu().numpy())
+            np.save(f'out{pid}_{savename}.npy', empty_subj.cpu().numpy())
 
     print('Done.')
-    return Dices.cpu().numpy()
+    #pidis = [int(p) for p in PIDS]
+    dices = np.concatenate((np.array(PID)[:,None], Dices.cpu().numpy()), axis=1)
+    np.save(f'dices_{savename}.npy', dices)
+    return dices
 
 
 

sandboxy.py
@@ -1,4 +1,9 @@
+#%%
 from Postprocessing import Compute3DDice
+import matplotlib.pyplot as plt
+import numpy as np
+from pathlib import Path
+import nibabel as nib
 
 #Compute3DDice(500017,'poem80_dts/unet_w/unet',80)
 #Compute3DDice(500017,'poem80_dts/deepmed_w_dts/deepmed_dts',80)
@@ -12,27 +17,67 @@ from Postprocessing import Compute3DDice
 #Compute3DDice(500017,'poem25_new/unet_dts/unet_dts',25, dev='cuda', step=2)
 
 #%%
-print(Compute3DDice('500062', 'poem25/deepmed1/deepmed', 25, batch=20, step=3, dev='cuda'))
-print(Compute3DDice('500062', 'poem25/deepmed_dts1/deepmed', 25, batch=20, step=3, dev='cuda'))
-print(Compute3DDice('500062', 'poem25/pnet1/pnet', 25, batch=20, step=3, dev='cuda'))
-print(Compute3DDice('500062', 'poem25/pnet_dts1/pnet', 25, batch=20, step=3, dev='cuda'))
-print(Compute3DDice('500062', 'poem25/unet1/unet', 25, batch=20, step=3, dev='cuda'))
-print(Compute3DDice('500062', 'poem25/unet_dts1/unet', 25, batch=20, step=3, dev='cuda'))
+print(Compute3DDice(500062, 'poem25/deepmed1/deepmed', 25, batch=20, step=3, dev='cuda',saveout=True, savename='dm1'))
+print(Compute3DDice(500062, 'poem25/deepmed_dts1/deepmed', 25, batch=20, step=3, dev='cuda',saveout=True, savename='dm1dts'))
+print(Compute3DDice(500062, 'poem25/pnet1/pnet', 25, batch=20, step=3, dev='cuda',saveout=True,savename='pn1'))
+print(Compute3DDice(500062, 'poem25/pnet_dts1/pnet', 25, batch=20, step=3, dev='cuda',saveout=True, savename='pn1dts'))
+print(Compute3DDice(500062, 'poem25/unet1/unet', 25, batch=20, step=3, dev='cuda',saveout=True, savename='un1'))
+print(Compute3DDice(500062, 'poem25/unet_dts1/unet', 25, batch=20, step=3, dev='cuda',saveout=True, savename='un1dts'))
 
 
 #%%
-Compute3DDice('500062', 'poem25/deepmed2/deepmed', 25, batch=20, bydim=2, step=3, dev='cuda')
-Compute3DDice('500062', 'poem25/deepmed_dts2/deepmed', 25, batch=20, bydim=2, step=3, dev='cuda')
-Compute3DDice('500062', 'poem25/pnet2/pnet', 25, batch=20, bydim=2, step=3, dev='cuda')
-Compute3DDice('500062', 'poem25/pnet_dts2/pnet', 25, batch=20, bydim=2, step=3, dev='cuda')
-Compute3DDice('500062', 'poem25/unet2/unet', 25, batch=20, bydim=2, step=3, dev='cuda')
-Compute3DDice('500062', 'poem25/unet_dts2/unet', 25, batch=20, bydim=2, step=3, dev='cuda')
+Compute3DDice(500062, 'poem25/deepmed2/deepmed', 25, batch=20, bydim=2, step=3, dev='cuda', saveout=True, savename='dm2')
+Compute3DDice(500062, 'poem25/deepmed_dts2/deepmed', 25, batch=20, bydim=2, step=3, dev='cuda', saveout=True, savename='dm2dts')
+Compute3DDice(500062, 'poem25/pnet2/pnet', 25, batch=20, bydim=2, step=3, dev='cuda', saveout=True, savename='pn2')
+Compute3DDice(500062, 'poem25/pnet_dts2/pnet', 25, batch=20, bydim=2, step=3, dev='cuda', saveout=True, savename='pn2dts')
+Compute3DDice(500062, 'poem25/unet2/unet', 25, batch=20, bydim=2, step=3, dev='cuda', saveout=True, savename='un2')
+Compute3DDice(500062, 'poem25/unet_dts2/unet', 25, batch=20, bydim=2, step=3, dev='cuda', saveout=True, savename='un2dts')
 
 
 #%%
-Compute3DDice('500062', 'poem25/deepmed3d/deepmed', 25, batch=10, step=3, dev='cuda')
-Compute3DDice('500062', 'poem25/deepmed_dts3d/deepmed', 25, batch=10, step=3, dev='cuda')
-Compute3DDice('500062', 'poem25/pnet3d/pnet', 25, batch=10, step=3, dev='cuda')
-Compute3DDice('500062', 'poem25/pnet_dts3d/pnet', 25, batch=10, step=3, dev='cuda')
-Compute3DDice('500062', 'poem25/unet3d/unet', 25, batch=10, step=3, dev='cuda')
-Compute3DDice('500062', 'poem25/unet_dts3d/unet', 25, batch=10, step=3, dev='cuda')
+Compute3DDice(500062, 'poem25/deepmed3d/deepmed', 25, batch=10, step=3, dev='cuda', saveout=True, savename='dm3D')
+Compute3DDice(500062, 'poem25/deepmed_dts3d/deepmed', 25, batch=10, step=3, dev='cuda', saveout=True, savename='dm3Ddts')
+Compute3DDice(500062, 'poem25/pnet3d/pnet', 25, batch=10, step=3, dev='cuda', saveout=True, savename='un3D')
+Compute3DDice(500062, 'poem25/pnet_dts3d/pnet', 25, batch=10, step=3, dev='cuda', saveout=True, savename='un3Ddts')
+Compute3DDice(500062, 'poem25/unet3d/unet', 25, batch=10, step=3, dev='cuda', saveout=True, savename='pn3D')
+Compute3DDice(500062, 'poem25/unet_dts3d/unet', 25, batch=10, step=3, dev='cuda', saveout=True, savename='pn3Ddts')
+
+
+#%%
+#check how it looks
+fajl = 'dm1dts'
+fil = 'out500022_'+fajl+'.npy'
+#which slices to plot:
+s1,s2,s3 = (150,50,50)
+
+img = np.load(fil)
+
+plt.figure()
+plt.subplot(2,3,1)
+plt.imshow(img[:,:,s3].squeeze().T, extent=(0,100,0,150), vmin=0, vmax=7)
+plt.axis('off')
+plt.subplot(2,3,2)
+plt.imshow(img[:,s2,:].squeeze(), vmin=0, vmax=7)
+plt.axis('off')
+plt.subplot(2,3,3)
+plt.imshow(img[s1,:,:].squeeze(), extent=(0,100,0,150), vmin=0, vmax=7)
+plt.axis('off')
+
+gt = list(Path('POEM', 'segms').glob(f'Cropped*{fil[3:9]}*'))[0]
+gt = nib.load(str(gt)).get_fdata()
+plt.subplot(2,3,4)
+plt.imshow(gt[:,:,-s3].squeeze().T, extent=(0,100,0,150), vmin=0, vmax=7)
+plt.axis('off')
+plt.subplot(2,3,5)
+plt.imshow(gt[:,-s2,:].squeeze(), vmin=0, vmax=7)
+plt.axis('off')
+plt.subplot(2,3,6)
+plt.imshow(gt[-s1,:,:].squeeze(), extent=(0,100,0,150), vmin=0, vmax=7)
+plt.axis('off')
+
+# %%
+fajl = 'un3D'
+dcs = 'dices_'+fajl+'.npy'
+dcs = np.load(dcs)
+print(dcs[0,1:])
+# %%